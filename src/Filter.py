483586224c38e54b758d5aa6bc8ac7c8375799db
kearnsw <kearns391@gmail.com>
2016-04-24 13:09:14 -0700

    Improved filtering using SW

---
+++ src/Filter.py
@@ -0,0 +1,18 @@
+import alignment
+
+
+class Filter:
+
+    def __init__(self, threshold):
+        self.stopwords = ['Zika', 'the', 'and', 'or']
+        self.threshold = threshold
+        self.match_len = 0
+
+    def check_duplicates(self, tweet1, tweet2):
+        tweet1 = tweet1.lower().split()
+        tweet2 = tweet2.lower().split()
+        self.match_len = alignment.water(tweet1, tweet2)
+        if self.match_len >= self.threshold:
+            return True
+        else:
+            return False
